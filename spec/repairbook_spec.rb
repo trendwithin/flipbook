9e4bae0702190e643e4568c55d83a2d187d4a9ff
Michael Becco <e4e6d4d5@gmail.com>
2015-03-30 22:00:49 -0700

    Add Test to Determine Fragileness of RegEx

--- spec/repairbook_spec.rb
+++ spec/repairbook_spec.rb
@@ -16,10 +16,19 @@ class TestRepairBook < MiniTest::Test
   def test_second_string
     expected = "Banking establishments are more dangerous than standing armies."
     gremlin = "armies. standing than dangerous more are establishments Banking"
+
+    assert_equal expected, @rb.flip_book(gremlin)
   end
 
-  def test_third_string
+  def test_dash_string
     expected = "People can have the Model T in any color - so long as it's black."
     gremlin = "black. it's as long so - color any in T Model the have can People"
+    assert_equal expected, @rb.flip_book(gremlin)
+  end
+
+  def test_ellipses
+    expected = "Made up sentence... Try it out!"
+    gremlin =  "out! it Try sentence... up Made"
+    assert_equal expected, @rb.flip_book(gremlin)
   end
 end
